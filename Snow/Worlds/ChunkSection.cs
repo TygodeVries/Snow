f4a9b4dcf2a76c18da360738fa3421e3e01722b8
TygodeVries <zttdevries@gmail.com>
2024-03-31 21:45:52 +0200

    Tested performance, Renderdistance, 1 player using around 150 MB of ram

---
+++ Snow/Worlds/ChunkSection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snow.Worlds
+{
+    public abstract class ChunkSection
+    {
+        public abstract short GetBlockAt(int x, int y, int z);
+    }
+}
